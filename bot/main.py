e16d700042cfbc2ae4a2e800011c43589e8985c1
auemaria <62420958+auemaria@users.noreply.github.com>
2022-10-05 00:37:17 +0800

    Жёско наговнокодила
    Обязательно исправлюсь

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,44 @@
 import telebot
+import schedule
+import threading
+from threading import Thread
+import time, datetime
+from datetime import datetime
+from time import sleep
 
 from telebot import types
 
 
-bot = telebot.TeleBot("5588347087:AAH3-qJmgvb6BYid-AzH4G_bEl2cb2pnsw0")
+some_id = -807099752
+bot = telebot.TeleBot("5588347087:AAH3-qJmgvb6BYid-AzH4G_bEl2cb2pnsw0", threaded=True)
 
 
-@bot.message_handler(commands=['start'])
-def start(message: types.Message):
-    pass
 
 
-@bot.chat_join_request_handler()
-def make_some(message: types.ChatJoinRequest):
-    print("request!")
-    bot.send_message(message.chat.id, 'I accepted a new user!')
-    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+def schedule_checker():  # рассылка
+    while True:
+        schedule.run_pending()
+        sleep(1)
+
+def func_to_run():
+    return bot.send_message(some_id, 'Привет, Никита додик?')
 
 
+@bot.message_handler(commands = ['start'])
+def welcome(message):
+
+
+    #keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Да")
+    item2 = types.KeyboardButton("Конечно")
+    markup.add(item1, item2)
+    print(message.chat.id)
+
+
+    bot.send_message(message.chat.id, 'Привет, Никита додик?', reply_markup=markup)
+
+#
+schedule.every().day.at("16:00").do(func_to_run)
+Thread(target=schedule_checker).start()
 bot.polling(none_stop=True)
