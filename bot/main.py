aae6d217bc8fd266e29f335db1edb7b89d2ace88
nikita0607 <ecfed205@gmail.com>
2022-10-05 17:07:09 +0800

    Add carulels for questions Remove parser's code

--- bot/main.py
+++ bot/main.py
@@ -4,13 +4,40 @@ import threading
 
 from threading import Thread
 from time import sleep
+from abc import ABC, abstractmethod
 
 from telebot import types
 
+from typing import Dict, Tuple
 
-some_id = -807099752
+
+
+
+class Chat:
+    def __init__(self, chat_id: int, admin_id: int):
+        self.chat_id = chat_id
+        self.admin_id = admin_id
+        self.questions = []
+
+
+class Carusel(ABC):
+    @abstractmethod
+    def step(self, message: types.Message) -> Tuple[str, types.ReplyKeyboardMarkup]:
+        pass
+
+
+class SelectorCarusel(Carusel):
+    def __init__(self):
+        self.step_num = 0
+ 
+    def step(self, message: types.Message) -> Tuple[str, types.ReplyKeyboardMarkup]:
+        if step == 0:
+            pass
+
+    
 bot = telebot.TeleBot("5588347087:AAH3-qJmgvb6BYid-AzH4G_bEl2cb2pnsw0", threaded=True)
-
+chats: Dict[int, Chat] = {}
+admins: Dict[int, Chat] = {}
 
 def schedule_checker():  # рассылка
     while True:
@@ -25,16 +52,36 @@ def func_to_run():
 @bot.message_handler(commands=['start'])
 def welcome(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Да")
-    item2 = types.KeyboardButton("Конечно")
-    markup.add(item1, item2)
+    item1 = types.KeyboardButton("Добавить вопрос")
+    markup.add(item1)
 
+    chat_id = message.chat.id
+
+    if chat_id in chats:
+        return
+    
+    chat = Chat(chat_id, message.from_user.id)
+    
     print(message.chat.id)
     print(message.from_user.id)
 
-    bot.send_message(message.chat.id, 'Привет, Никита додик?', reply_markup=markup)
+    chats[chat_id] = chat
+    admins[chat.admin_id] = chat
+    bot.send_message(chat.admin_id, 'Welcome!', reply_markup=markup)
+    
+
+@bot.message_handler(func=lambda message: message.text=="Добавить вопрос")
+def add_question(message: types.Message):
+    if message.from_user.id not in admins:
+        return
+    
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кружочки")
+    item2 = types.KeyboardButton("ке кружочки")
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id, "Next sppee", reply_markup=markup)
 
 
-schedule.every().day.at("16:00").do(func_to_run)
 Thread(target=schedule_checker).start()
 bot.polling(none_stop=True)
--- bot/question.py
+++ bot/question.py
@@ -1,25 +1,5 @@
-import docx2txt
-
 from enum import Enum
-from typing import IO, List, Union, Tuple
-from abc import ABC, abstractmethod
-
-
-class QuestionParser(ABC):
-    type = ''
-
-    @classmethod
-    @abstractmethod
-    def parse(cls, text: str) -> Tuple['Question', str]:
-        pass
-
-
-class SelectorParser(QuestionParser):
-    type = 'selector'
-
-    @classmethod
-    def parse(cls, text: str) -> Tuple['Question', str]:
-        pass
+from typing import Union
 
 
 class QuestionTypes(Enum):
@@ -37,22 +17,3 @@ class Question:
     def __init__(self, _type=None):
         self.type = _type
 
-
-
-class QuestionStringParser:
-    @classmethod
-    def parse_questions(cls, text: str) -> List[Question]:
-        questions = []
-
-        while (parser := QuestionTypes.find_type(text)) is not None:
-            quest, text = parser.parse(text)
-            questions.append(quest)
-
-        return questions
-
-
-class QuestionDocxParser:
-    @classmethod
-    def parse_questions(cls, file_name: str) -> List[Question]:
-        text = docx2txt.process(file_name, '/images')
-        return QuestionStringParser.parse_questions(text)
